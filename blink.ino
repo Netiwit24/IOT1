f61713c33c87961b1c686d36a72c27b0ab88c046
Netiwit24 <neativit04@gmail.com>
2017-10-02 11:18:56 +0700

    Add files via upload

---
+++ blink.ino
@@ -0,0 +1,16 @@
+int led = 13; //กำหนดให้ Node_LED แทนชื่อ digital I/O ขาที่ 2 ของ NodeMCU (Built in LED)
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(led,OUTPUT); //กำหนดให้ Node_LED มีรูปแบบการทำงานในโหมดเอาต์พุต
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  digitalWrite(led,HIGH);  //กำหนดให้ส่งสัญญาณเอาต์พุต HIGH (3.3V) ออกทางขา Node_LED 
+  delay(200); //หน่วงเวลา 200 มิลลิวินาที
+  digitalWrite(led,LOW); //กำหนดให้ส่งสัญญาณเอาต์พุต LOW (0V) ออกทางขา Node_LED 
+  delay(200); //หน่วงเวลา 200 มิลลิวินาที
+
+}
